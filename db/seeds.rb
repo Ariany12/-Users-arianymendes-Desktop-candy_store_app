bd91f13485c71c02b4b8c1e4537d188af15ac76a
ARIANY <ariany_mendes@yahoo.com>
2020-03-06 15:46:38 -0500

    update in the route,controller and json file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# products = Product.new({name: "jujuba", price: 2, flavor: "sweet", image: "none"}) 
+# products.save 
+# products = Product.new({name: "bala_bombom", price: 2, flavor: "sweet", image: "none"})
+# products.save 
+# products = Product.new({name: "amendoim", price: 2, flavor: "sweet", image: "none"})
+# products.save 
+
+# 10.times do 
+#   {
+#     products = Product.new({name: Faker::Food.fruits, price: Faker::Number.within(range: 1..10), flavor: Faker::Food.spice, image: "none"})
+#   }
